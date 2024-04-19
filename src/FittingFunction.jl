6bb5800e8fa0676286fdb09809da9925a73c3e91
Stefano Covino <stefano.covino@inaf.it>
2024-04-19 16:20:04 +0200

    190424 commit

--- src/FittingFunction.jl
+++ src/FittingFunction.jl
@@ -2,6 +2,7 @@ module FittingFunction
 
 
 export PL
+export SBPL
 
 
 
@@ -24,6 +25,33 @@ function PL(E,N,β;E₀ = 1.)
     spec = .^(E./E₀,β)
     return N .* spec
 end
+
+
+
+
+
+"""
+    SBPL(E,N,α₁,α₂,bE)
+
+Computes a smoothly joint broken power-law with spectral index '$\alpha_1$' and '$\alpha_2$', a break at 'Eb' and normalization 'N' at input 'E'.
+
+
+# Examples
+```jldoctest
+SBPL(6.,1.,-1.,1.5,5.)
+
+# output
+
+0.26290682760247974
+```
+""
+function SBPL(E,N,α₁,α₂,Eb)
+    f1 = PL(E,N,α₁)
+    n = PL(Eb,N,α₁) ./ PL(Eb,1.0,α₂)
+    f2 = PL(E,n,α₂)
+    return ifelse.(E .<= Eb, f1, f2)
+end
+
 
 
 end
